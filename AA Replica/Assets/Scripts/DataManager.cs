f824891caeb497b4e656072ded6df3c4f3ef1d5c
NSParadox <76891137+N8Sulliv@users.noreply.github.com>
2021-03-12 14:51:11 -0600

    Some more

---
+++ AA Replica/Assets/Scripts/DataManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DataManager : MonoBehaviour
+{
+    public static int score;
+    public static float rotationSpeed;
+    public static float pinSpeed;
+
+    public Slider rotateSlider;
+    public Slider pinSlider;
+    public Text rotationText;
+    public Text pinText;
+
+    public void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void Update()
+    {
+        score = Score.pinCount;
+    }
+
+    public void setRotateSpeed()
+    {
+        rotationSpeed = rotateSlider.value;
+    }
+
+    public void setPinSpeed()
+    {
+        pinSpeed = pinSlider.value;
+    }
+
+    public void setPinText()
+    {
+        pinText.text = "Pin Speed: " + pinSlider.value.ToString();
+    }
+
+    public void setRotationText()
+    {
+        rotationText.text = "Rotator Speed: " + rotateSlider.value.ToString();
+    }
+}
---
+++ AA Replica/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+
+    public void back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void restart()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void backToStart()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
